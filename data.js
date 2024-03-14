8d6dc8b77a7509faef44ea2b845355bc7896105d
Aldian Umbu <53179761+altom11@users.noreply.github.com>
2024-03-14 18:26:27 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,86 @@
+export const mapClasses = [
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+  ];
+  export const previews = [
+    {
+      img: "./assets/main-1.jpg",
+      title: "Berjiwa entrepreneur, kreatif, inovatif & problem solving",
+      tags: "Mempunyai Tiga Profil Lulusan",
+      description:
+        "Membekali mahasiswa dengan keterampilan teknologi dan bisnis yang diperlukan untuk menghadapi era digital.",
+    },
+    {
+      img: "./assets/main-2.jpg",
+      title: "Profil Lulusan Digital Marketing",
+      tags: "Strategi Pemasaran Digital, Website, Kreatiif",
+      description:
+        "Profil lulusan digital marketing yang memiliki keahlian strategis, selalu mengikuti perkembangan teknologi terkini, dan mampu berinovasi dalam penerapan strategi pemasaran digital, sesuai dengan tuntutan zaman saat ini.",
+    },
+    {
+      img: "./assets/main-3.jpg",
+      title: "Profil Lulusan Startup Entrepreneur",
+      tags: "Kewirausahaan, Inovasi, Digitalisasi Bisnis",
+      description:
+        "Membangun karakter pemimpin mampu beradaptasi dengan cepat terhadap perubahan, memungkinkan mereka untuk mengembangkan ide bisnis startup dengan sukses di era digital.",
+    },
+    {
+      img: "./assets/main-4.jpg",
+      title: "Profil Lulusan Data Analyst",
+      tags: "Analisis Data, Visualisasi Data, Pengambilan Keputusan",
+      description:
+        "Memiliki keahlian analitis yang kuat ditambah dengan kemampuan visualisasi data yang memukau dan kemampuan pengambilan keputusan yang terukur di berbagai bidang industri.",
+    },
+    {
+      img: "./assets/main-5.jpg",
+      title: "Fokus pada pemecahan masalah UMKM atau perusahaan",
+      tags: "Model Pembelajaran berbasis produk",
+      description:
+        "pembelajaran yang menempatkan proyek atau tugas berbasis proyek sebagai fokus utama. proyek ini dapat bervariasi mulai dari studi kasus industri nyata, pengembangan produk atau solusi, hingga konsep pemasaran atau riset pasar.",
+    },
+    {
+      img: "./assets/main-6.jpg",
+      title: "Mitra Kolaborasi",
+      tags: "",
+      description:
+        "Program studi bekerja sama dengan perusahaan, lembaga sertifikasi, asosiasi bisnis digital maupun kampus. Daftar kerjasama: Naruna, Teknologi Kartu Indonesia, Wisanka, Rimba Linux, DqLab, APBISDI, dll",
+        
+    },
+    {
+      img: "./assets/main-7.jpg",
+      title: "Pitcher",
+      tags: "Innovative Textiles, Web Presentation",
+      description:
+        "Web presentation of innovative textiles that redefine the boundaries of modern fashion.",
+    },
+    {
+      img: "./assets/main-8.jpg",
+      title: "MindSpace",
+      tags: "Augmented Reality, Fashion Design",
+      description:
+        "Augmented reality in fashion design, creating immersive web experiences for users.",
+    },
+    {
+      img: "./assets/main-9.jpg",
+      title: "The Athletix",
+      tags: "Virtual Fashion, Graphic Design",
+      description:
+        "Virtual fashion collections displayed through stunning graphic design and web aesthetics.",
+    },
+    {
+      img: "./assets/main-10.jpg",
+      title: "All Work",
+      tags: "Minimalist Aesthetics, Fashion Forward",
+      description:
+        "Fashion forward web design encapsulating the essence of minimalist aesthetics and innovation.",
+    },
+  ];
+  
